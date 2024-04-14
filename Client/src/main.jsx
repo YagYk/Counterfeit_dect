40eb6f8b87770247d48c4bd6bdbff6800f0077aa
Yagyansh Khandelwal <113237147+YagYk@users.noreply.github.com>
2024-04-14 13:17:02 +0530

    Add files via upload

---
+++ Client/src/main.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
+
+
+import App from './Application';
+import Application from './Application';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <ThirdwebProvider desiredChainId={ChainId.sepolia}> 
+    <Router>
+      
+        <Application />
+      
+    </Router>
+  </ThirdwebProvider> 
+)
