4414ebce94c336ce71ec86f9e5bd786450ae3ca7
Yagyansh Khandelwal <113237147+YagYk@users.noreply.github.com>
2024-04-14 13:12:41 +0530

    Add files via upload

---
+++ Client/vite.config.js
@@ -0,0 +1,17 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // ...
+  build: {
+    target: 'esnext', // you can also use 'es2020' here
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'esnext', // you can also use 'es2020' here
+    },
+  },
+  base: './',
+});
